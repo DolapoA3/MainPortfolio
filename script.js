751847fe479e0984b7642e6da795b537753ba94e
LeoKing33 <lapo8387@gmail.com>
2022-10-19 15:17:35 -0500

    Images setup

--- script.js
+++ script.js
@@ -57,30 +57,28 @@ document.body.addEventListener('mouseleave', () => {
 
 const mainBtns = document.querySelectorAll('.main-btn');
 
-mainBtns.forEach(btn => {
-let ripple;
+mainBtns.forEach((btn) => {
+  let ripple;
 
-btn.addEventListener('mouseenter', (e) => {
-  const left = e.clientX - e.target.getBoundingClientRect().left;
-  const top = e.clientY - e.target.getBoundingClientRect().top;
+  btn.addEventListener('mouseenter', (e) => {
+    const left = e.clientX - e.target.getBoundingClientRect().left;
+    const top = e.clientY - e.target.getBoundingClientRect().top;
 
-  ripple = document.createElement('div');
-  ripple.classList.add('ripple');
-  ripple.style.left = `${left}px`;
-  ripple.style.top = `${top}px`;
-  btn.prepend(ripple);
+    ripple = document.createElement('div');
+    ripple.classList.add('ripple');
+    ripple.style.left = `${left}px`;
+    ripple.style.top = `${top}px`;
+    btn.prepend(ripple);
+  });
+
+  btn.addEventListener('mouseleave', () => {
+    btn.removeChild(ripple);
+  });
 });
-
-btn.addEventListener('mouseleave', () => {
-  btn.removeChild(ripple);
-});
-})
-
-
 
 const aboutMeText = document.querySelector('.about-me-text');
 const aboutMeTextContent =
-  'I am an aspiring software developer that has grown a strong passion for programming. I consistently create websites & apps for the best user experience. Contact me here. :)';
+  'I am an aspiring software developer that has grown a strong passion for programming. I consistently create websites & apps for the best user experience. Just contact me. :)';
 
 Array.from(aboutMeTextContent).forEach((char) => {
   const span = document.createElement('span');
@@ -89,5 +87,32 @@ Array.from(aboutMeTextContent).forEach((char) => {
 
   span.addEventListener('mouseenter', (e) => {
     e.target.style.animation = 'aboutMeTextAnim 10s infinite';
+  });
+});
+
+const container = document.querySelector('.container');
+const projects = document.querySelectorAll('.project');
+
+projects.forEach((project) => {
+  project.addEventListener('mouseenter', () => {
+    project.firstElementChild.style.top = `-${
+      project.firstElementChild.offsetHeight - project.offsetHeight + 20
+    }px`;
+  });
+
+  project.addEventListener('mouseleave', () => {
+    project.firstElementChild.style.top = '2rem';
+  });
+
+  project.addEventListener('click', () => {
+    const bigImgWrapper = document.createElement('div');
+    bigImgWrapper.className = 'project-img-wrapper';
+    container.appendChild(bigImgWrapper);
+
+    const bigImg = document.createElement('img');
+    bigImg.className = 'project-img';
+    const imgPath = project.firstElementChild.getAttribute('src').split('.')[0];
+    bigImg.setAttribute('src', `${imgPath}-big.jpg`);
+    bigImgWrapper.appendChild(bigImg);
   });
 });
